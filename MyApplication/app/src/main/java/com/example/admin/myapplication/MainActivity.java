d01eb72d5728c3d4829266e1b7d2cbc66318d13b
Luomilande <2500870078@qq.com>
2018-12-25 00:59:57 +0800

    添加语音提示
    给遥感添加语音提示

--- MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.admin.myapplication;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,17 +19,20 @@ import com.example.admin.myapplication.RockerView.Direction;
 import com.example.admin.myapplication.RockerView.DirectionMode;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;
     private boolean run = false;
     private final Handler handler = new Handler();
     private int a=1,b=1;
+    public String info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initrokerview();
         run = true;
         handler.postDelayed(task, 3000);
     }
@@ -65,6 +70,47 @@ public class MainActivity extends AppCompatActivity {
             imgbtn.setBackgroundResource(R.drawable.fly_no);
         }
     }
+
+
+    public void init(){
+
+        switch (info)
+        {
+            case "背景":
+                mp=MediaPlayer.create(this,R.raw.dy);
+                break;
+            case "前":
+                mp=MediaPlayer.create(this,R.raw.qian);//前飞
+                break;
+            case "后":
+                mp=MediaPlayer.create(this,R.raw.hou);//后飞
+                break;
+            case "左":
+                mp=MediaPlayer.create(this,R.raw.zuo);//左飞
+                break;
+            case "右":
+                mp=MediaPlayer.create(this,R.raw.you);//右飞
+                break;
+            case "连":
+                mp=MediaPlayer.create(this,R.raw.lian);//没连上
+                break;
+        }
+        try
+        {
+            mp.prepare();
+        }catch (IllegalStateException e)
+        {
+            e.printStackTrace();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        if(!mp.isPlaying())
+        {
+            mp.start();
+        }
+    }
 //    public boolean onTouchEvent(MotionEvent event) {
 //
 ////获得触摸的坐标
@@ -92,48 +138,68 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
 
-//    public void initrokerview(){
-//        //找到RockerView控件
-//        RockerView roker=(RockerView) findViewById(R.id.RockerView);
-//        //实时监测摇动方向
-//        roker.setOnShakeListener(DirectionMode.DIRECTION_8, new OnShakeListener() {
-//            //开始摇动时要执行的代码写在本方法里
-//            @Override
-//            public void onStart() {
-//
-//            }
-//            //结束摇动时要执行的代码写在本方法里
-//            @Override
-//            public void onFinish() {
-//                Toast.makeText(MainActivity.this, "已复位", 0).show();
-//            }
-//            //摇动方向时要执行的代码写在本方法里
-//            @Override
-//            public void direction(Direction direction) {
-//                RockerView.Direction.
-//                if (direction == RockerView.Direction.DIRECTION_CENTER){
-//                    tv.setText("中心");
-//                }else if (direction == RockerView.Direction.DIRECTION_DOWN){
-//                    tv.setText("下");
-//                }else if (direction == RockerView.Direction.DIRECTION_LEFT){
-//                    tv.setText("左");
-//                }else if (direction == RockerView.Direction.DIRECTION_UP){
-//                    tv.setText("上");
-//                }else if (direction == RockerView.Direction.DIRECTION_RIGHT){
-//                    tv.setText("右");
-//                }else if (direction == RockerView.Direction.DIRECTION_DOWN_LEFT){
-//                    tv.setText("左下");
-//                }else if (direction == RockerView.Direction.DIRECTION_DOWN_RIGHT){
-//                    tv.setText("右下");
-//                }else if (direction == RockerView.Direction.DIRECTION_UP_LEFT){
-//                    tv.setText("左上");
-//                }else if (direction == RockerView.Direction.DIRECTION_UP_RIGHT){
-//                    tv.setText("右上");
-//                }
-//
-//            }
-//        });
-//    }
+    public void initrokerview(){
+        //找到RockerView控件
+        RockerView roker=(RockerView) findViewById(R.id.RockerView);
+        //实时监测摇动方向
+        roker.setOnShakeListener(DirectionMode.DIRECTION_8, new OnShakeListener() {
+            //开始摇动时要执行的代码写在本方法里
+            @Override
+            public void onStart() {
+
+            }
+            //结束摇动时要执行的代码写在本方法里
+            @Override
+            public void onFinish() {
+                info=null;
+                //Toast.makeText(MainActivity.this, "已复位", Toast.LENGTH_SHORT).show();
+            }
+            //摇动方向时要执行的代码写在本方法里
+            @Override
+            public void direction(Direction direction) {
+                if (direction == RockerView.Direction.DIRECTION_CENTER){
+                    //tv.setText("中心");
+                }else if (direction == RockerView.Direction.DIRECTION_DOWN){
+                    //tv.setText("下");
+                    if (info!="后")
+                    {
+                        info="后";
+                        init();
+                    }
+                }else if (direction == RockerView.Direction.DIRECTION_LEFT){
+                    //tv.setText("左");
+                    if (info!="左")
+                    {
+                        info="左";
+                        init();
+                    }
+                }else if (direction == RockerView.Direction.DIRECTION_UP){
+                    //tv.setText("上");
+                    if (info!="前")
+                    {
+                        info="前";
+                        init();
+                    }
+                }else if (direction == RockerView.Direction.DIRECTION_RIGHT){
+                    //tv.setText("右");
+                    if (info!="右")
+                    {
+                        info="右";
+                        init();
+                    }
+                }else if (direction == RockerView.Direction.DIRECTION_DOWN_LEFT){
+                    //tv.setText("左下");
+                }else if (direction == RockerView.Direction.DIRECTION_DOWN_RIGHT){
+                    //tv.setText("右下");
+                }else if (direction == RockerView.Direction.DIRECTION_UP_LEFT){
+                    //tv.setText("左上");
+                }else if (direction == RockerView.Direction.DIRECTION_UP_RIGHT){
+                    //tv.setText("右上");
+                }
+
+            }
+        });
+    }
     public void sxTouch(View view)
     {
         ImageButton sxbtn= findViewById(R.id.lximgbtn);
