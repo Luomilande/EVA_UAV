fa03952985b246a2c347b4c8672d25b1e7d73a85
Luomilande <2500870078@qq.com>
2018-12-31 16:14:23 +0800

    添加蓝牙连接，并开放设置功能
    设置用于修改连接无人机蓝牙MAC地址

--- MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.admin.myapplication;
 
+
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.UUID;
 import android.text.Html;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import com.example.admin.myapplication.RockerView.OnShakeListener;
@@ -22,6 +38,37 @@ import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.Random;
+/**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *            佛曰:
+ *                   写字楼里写字间，写字间里程序员；
+ *                   程序人员写程序，又拿程序换酒钱。
+ *                   酒醒只在网上坐，酒醉还来网下眠；
+ *                   酒醉酒醒日复日，网上网下年复年。
+ *                   但愿老死电脑间，不愿鞠躬老板前；
+ *                   奔驰宝马贵者趣，公交自行程序员。
+ *                   别人笑我忒疯癫，我笑自己命太贱；
+ *                   不见满街漂亮妹，哪个归得程序员？
+ */
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mp;//定义MediaPlayer
@@ -30,10 +77,27 @@ public class MainActivity extends AppCompatActivity {
     private final Handler handler = new Handler();
     private int a=1,b=1;
 
+    public String address="20:F7:7C:85:5E:84";//蓝牙mac地址
+    public UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");//定义UUID
+    public BluetoothSocket socket;//定义蓝牙socket
+    public OutputStream out;//定义输出流
+    public byte[]data=new byte[34];//定义通信数组
+    public void initdata() {//通信数组赋值
+        data[0] = (byte) 0xAA;
+        data[1] = (byte) 0xC0;
+        data[2] = (byte) 0x1C;
+
+        data[3] = (byte) (300 >> 8);
+        data[4] = (byte) (300 & 0xff);
+        data[31] = (byte) 0x1C;
+        data[32] = (byte) 0x0D;
+        data[33] = (byte) 0x0A;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initdata();
        // initrokerview();
         //run = true;
        //handler.postDelayed(task, 3000);
@@ -235,6 +299,94 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
+    /**
+     * 初始化蓝牙相关操作
+     * 获取蓝牙设备，连接蓝牙
+     */
+    private class ConnectThread implements  Runnable{
+        @Override
+        public void run() {
+            //创建线程
+            BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();//获取蓝牙适配器
+            BluetoothDevice device = adapter.getRemoteDevice(address);//获取蓝牙设备
+            if (device == null) {
+                Log.e("提示","获取蓝牙失败");
+            }
+            try {
+                //连接服务器
+                socket = (BluetoothSocket) device.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(device, 1);
+                //获取socket
+                socket.connect();
+                //通过输出流发送数据给服务端
+                out = socket.getOutputStream();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("提示","连接异常");
+            }
+            //socket=device.createRfcommSocketToServiceRecord(uuid);
+        }
+    }
+    /**
+     * 点击按钮启动线程
+     * @param view
+     */
+    public void  btnqd(View view){
+       // Thread t=new Thread(new ConnectThread());
+       // t.start();
+        BluetoothAdapter blueadapter=BluetoothAdapter.getDefaultAdapter();
+        if (blueadapter.isEnabled()){
+            //Toast.makeText(getApplicationContext(),"蓝牙状态：开",Toast.LENGTH_SHORT).show();
+            new Thread(new ConnectThread()).start();
+        }else{
+            Toast.makeText(getApplicationContext(),"蓝牙状态：关",Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    /**
+     * 点击按钮发送数据
+     * @param view
+     */
+    public void  btnly(View view){
+        try{
+            out.write(data);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * 设置弹框修改连接蓝牙MAC地址
+     * @param view
+     */
+    public void alert_edit(View view){
+        final EditText et = new EditText(this);
+        new AlertDialog.Builder(this).setTitle("蓝牙MAC地址更改:")
+                .setIcon(android.R.drawable.sym_def_app_icon)
+                .setView(et)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //按下确定键后的事件
+                        String telReg="([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}";
+                        if (!et.getText().toString().equals("")&&et.getText().toString().matches(telReg))
+                        {
+                            address=et.getText().toString();
+                            Toast.makeText(getApplicationContext(),"蓝牙地址更改为:"+et.getText().toString(),Toast.LENGTH_SHORT).show();
+                        }else
+                        {
+                            Toast.makeText(getApplicationContext(),"提示:蓝牙MAC地址格式错误！",Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(getApplicationContext(),"蓝牙地址:取消更改",Toast.LENGTH_SHORT).show();
+                        }
+
+                       // Toast.makeText("", et.getText().toString(),Toast.LENGTH_LONG).show();
+                    }
+                }).setNegativeButton("取消",null).show();
+    }
 }
